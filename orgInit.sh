1e1220c96daa1d5d9640f5f6e5c8495331807012
zhou059 <zhou059@gmail.com>
2022-09-19 13:40:11 -0700

    init

--- orgInit.sh
+++ orgInit.sh
@@ -1,21 +1,11 @@
 # Create the demo org
-sfdx shane:org:create -f config/project-scratch-def.json -d 30 -s --wait 60 --userprefix admin -o transaction.demo
-#sfdx force:org:create -f config/project-scratch-def.json -a transaction-security-demo
-
-# Updates the Transaction Security Policies to contain this scratch orgs username
-sfdx shane:tsp:username:update
+sfdx shane:org:create -f config/project-scratch-def.json -d 2 -s --wait 60 --userprefix platform -o encryption.demo
 
 # Push the metadata into the new scratch org.
 sfdx force:source:push
-
-# Assign user the permset
-sfdx force:user:permset:assign -n TransactionSecurity
 
 # Set the default password.
 sfdx shane:user:password:set -g User -l User -p salesforce1
 
 # Open the org.
 sfdx force:org:open
-
-# Import the data required by the demo
-sfdx automig:load --inputdir ./data
